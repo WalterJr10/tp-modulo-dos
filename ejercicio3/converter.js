6f13a6336268f6547c3791e883fd338babbbbf10
Walter Agustín <101841775+WalterJr10@users.noreply.github.com>
2024-07-07 15:31:04 -0300

    last-commit

--- ejercicio3/converter.js
+++ ejercicio3/converter.js
@@ -7,28 +7,147 @@ class Currency {
 
 class CurrencyConverter {
     constructor() {
-        this.apiUrl = 'https://www.frankfurter.app/';
+        this.apiUrl = 'api.frankfurter.app';
         this.currencies = [];
     }
 
-    getCurrencies(apiUrl) {
-        const urlPeticion = `${this.url}${apiUrl}`;
-        fetch(urlPeticion)
-            .then(response => response.json)
-            .then(data => {
-                console.log(urlPeticion)
-                console.log(data)
-            })
-        this.currencies.push(1);
-        console.log(this.concurrencies)
-    }
+    async getCurrencies() {
+        const urlPeticion = `https://${this.apiUrl}/currencies`;
 
-    convertCurrency(amount, fromCurrency, toCurrency) {
-        const latestUrl = `${this.apiUrl}/latest`;
+        const resp = await fetch(urlPeticion)
+        const datas = await resp.json()
+
+        /*
+            convertir el objeto obtenido de la API a Array para crear instancias de Currency
+            al iterar
+        */  
+        const arrayDatas = Object.entries(datas)
         
+        for (let i = 0; i < arrayDatas.length; i++) {
+            const curenci = new Currency();
+            curenci.code = arrayDatas[i][0]
+            curenci.name = arrayDatas[i][1]
+            this.currencies.push(curenci)
+        }
 
-
+        
     }
+
+    async convertCurrency(amount, fromCurrency, toCurrency) {
+        const fromCy = fromCurrency.code
+        const toCy = toCurrency.code
+        const latestUrl = `https://${this.apiUrl}/latest?amount=${amount}&from=${fromCy}&to=${toCy}`;
+        
+        const resp = await fetch(latestUrl)
+        const data = await resp.json()
+
+        let monto = 0;
+        if(fromCy == toCy){
+            monto = amount;
+        } else {
+            switch (toCy) {
+                case "AUD": //1
+                    monto = `${data.rates.AUD}`
+                    break;
+                case "BGN":
+                    monto = `${data.rates.BGN}`
+                    break;
+                case "BRL":
+                    monto = `${data.rates.BRL}`
+                    break;
+                case "CAD":
+                    monto = `${data.rates.CAD}`
+                    break;
+                case "CHF":
+                    monto = `${data.rates.CHF}`
+                    break;
+                case "CNY":
+                    monto = `${data.rates.CNY}`
+                    break;
+                case "CZK":
+                    monto = `${data.rates.CZK}`
+                    break;
+                case "DKK":
+                    monto = `${data.rates.DKK}`
+                    break;
+                case "EUR": 
+                    monto = `${data.rates.EUR}`
+                    break;
+                case "GBP": 
+                    monto = `${data.rates.GBP}`
+                    break;
+                case "HKD":
+                    monto = `${data.rates.HKD}`
+                    break;
+                case "HUF":
+                    monto = `${data.rates.HUF}`
+                    break;
+                case "IDR":
+                    monto = `${data.rates.IDR}`
+                    break;
+                case "ILS":
+                    monto = `${data.rates.ILS}`
+                    break;
+                case "INR":
+                    monto = `${data.rates.INR}`
+                    break;
+                case "ISK":
+                    monto = `${data.rates.ISK}`
+                    break;
+                case "JPY":
+                    monto = `${data.rates.JPY}`
+                    break;
+                case "KRW": 
+                    monto = `${data.rates.KRW}`
+                    break;
+                case "MXN": 
+                    monto = `${data.rates.MXN}`
+                    break;
+                case "MYR": 
+                    monto = `${data.rates.MYR}`
+                    break;
+                case "NOK":
+                    monto = `${data.rates.NOK}`
+                    break;
+                case "NZD":
+                    monto = `${data.rates.NZD}`
+                    break;
+                case "PHP":
+                    monto = `${data.rates.PHP}`
+                    break;
+                case "PLN":
+                    monto = `${data.rates.PLN}`
+                    break;
+                case "RON":
+                    monto = `${data.rates.RON}`
+                    break;
+                case "SEK":
+                    monto = `${data.rates.SEK}`
+                    break;
+                case "SGD": 
+                    monto = `${data.rates.SGD}`
+                    break;
+                case "THB": 
+                    monto = `${data.rates.THB}`
+                    break;
+                case "TRY": 
+                    monto = `${data.rates.TRY}`
+                    break;
+                case "USD": 
+                    monto = `${data.rates.USD}`
+                    break;
+                case "ZAR": 
+                    monto = `${data.rates.ZAR}`
+                    break;
+                default:
+                    break;
+            }
+        }
+        const montoFinal = Number(monto)
+        return montoFinal;
+
+    }  
+
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
